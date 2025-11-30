44a7c81008c1c472246262e468e872b20abebc52
hkmtakgn <hikmetakgun94@gmail.com>
2025-11-30 06:01:06 +0300

    'configured-edit-button'

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9a#mii$&945r3cjim@f04-yk+j&-y!@)^yw6b%i^km6dy(&s8o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,8 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-import os
-STATIC_ROOT = os.path.join (BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from todo.models import Task
 
 def todo_home_views (request,task_slug=None,task_id=None,search_slug=None):
+	edit_task = None
 	if search_slug:
 		todo_form = TodoForm ()
 		todos = Task.objects.filter (task_name__icontains=request.POST.get('task_search'))
@@ -14,22 +15,23 @@ def todo_home_views (request,task_slug=None,task_id=None,search_slug=None):
 		todos=todos.order_by("-pk")
 	else:
 		todos = None
+	if task_id and task_slug:
+		edit_task = get_object_or_404 (Task,task_slug=task_slug,id=task_id)
+		todo_form = TodoForm (instance=edit_task)
 	if request.method == "POST":
-		todo_form = TodoForm (request.POST)
+		todo_form = TodoForm (request.POST,instance=edit_task)
 		if todo_form.is_valid():
 			todo_form.save(commit=False)
-			task_set,created = Task.objects.get_or_create(task_name=todo_form.cleaned_data.get('task_name'))
-			if created:
-				messages.success (request,"Success!")
-			else:
+			is_exists = Task.objects.filter(task_name=todo_form.cleaned_data.get('task_name')).exists()
+			if is_exists:
 				messages.warning(request,'Already exists!')
+			else:
+				todo_form.save()
+				messages.success (request,"Success!")
 			return redirect ("todo_home")
 	else:
 		if task_slug == None and task_id == None:
 			todo_form = TodoForm()
-		elif task_id and task_slug:
-				forEdit = get_object_or_404 (Task,task_slug=task_slug,id=task_id)
-				todo_form = TodoForm (instance=forEdit)
 	context = dict(
 		todo_form=todo_form,
 		todos=todos,
